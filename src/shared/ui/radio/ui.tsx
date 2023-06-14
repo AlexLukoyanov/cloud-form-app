267a7dcd166db5ec50f0070f1b684246c09871f5
AlexLukoyanov <lukoav@gmail.com>
2023-06-14 19:06:50 +0300

    change(shared): change styles for radio button

--- src/shared/ui/radio/ui.tsx
+++ src/shared/ui/radio/ui.tsx
@@ -24,8 +24,7 @@ export const RadioButton = forwardRef(
 );
 
 const Container = styled.div`
-  display: flex;
-  align-items: center;
+  position: relative;
 
   label {
     cursor: pointer;
@@ -33,42 +32,43 @@ const Container = styled.div`
     align-items: center;
   }
 
-  span {
-    display: none;
-  }
-
   input {
     position: absolute;
     opacity: 0;
-    cursor: pointer;
     z-index: -1;
-  }
-
-  input:checked {
-    & + label::before {
-      background-image: url("data:image/svg+xml,%3Csvg width='10' height='10' viewBox='0 0 4 4' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Ccircle cx='2.00002' cy='1.9999' r='1.6' fill='white'/%3E%3C/svg%3E");
-      background-color: ${(p) => p.theme.colors.purple};
-      border: 1.5px solid ${(p) => p.theme.colors.purple};
-      background-position: center;
-      background-repeat: no-repeat;
-    }
-  }
-
-  input:focus {
-    & + label::before {
-      box-shadow: 0 0 3px ${(p) => p.theme.colors.purple};
-    }
   }
 
   label::before {
     content: "";
-    display: flex;
-    align-items: center;
-    justify-content: center;
     width: 16px;
     height: 16px;
     border-radius: 50%;
     border: 1.5px solid ${(p) => p.theme.colors.alpha[24]};
     margin-right: 5px;
+  }
+
+  input:checked {
+    & + label::before {
+      background-color: ${(p) => p.theme.colors.purple};
+      border: 1.5px solid ${(p) => p.theme.colors.purple};
+    }
+  }
+
+  input:checked {
+    & + label::after {
+      position: absolute;
+      content: "";
+      width: 9px;
+      height: 9px;
+      border-radius: 50%;
+      left: 5px;
+      background-color: ${(p) => p.theme.colors.white};
+    }
+
+    input:focus {
+      & + label::before {
+        box-shadow: 0 0 3px ${(p) => p.theme.colors.purple};
+      }
+    }
   }
 `;
